1939fa07486070f64c9a3e2b629220b3a4de98ec
z5539467 <z5539467@ad.unsw.ed.au>
2025-11-21 14:23:46 +1100

    a new update

---
+++ scripts/LoraMoE/Eval/.ipynb_checkpoints/Eval_all-checkpoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+bash ./scripts/CLMoE/Eval/1_eval_recognition.sh
+bash ./scripts/CLMoE/Eval/2_eval_location.sh
+bash ./scripts/CLMoE/Eval/3_eval_judge.sh
+bash ./scripts/CLMoE/Eval/4_eval_commonsense.sh
+bash ./scripts/CLMoE/Eval/5_eval_count.sh
+bash ./scripts/CLMoE/Eval/6_eval_action.sh
+bash ./scripts/CLMoE/Eval/7_eval_color.sh
+bash ./scripts/CLMoE/Eval/8_eval_type.sh
+bash ./scripts/CLMoE/Eval/9_eval_subcategory.sh
+bash ./scripts/CLMoE/Eval/10_eval_causal.sh
